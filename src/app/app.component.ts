727fecb2b44b1683683121915b50ad2ceb7d0fdc
ArturSlasher <archiblablablapicher@gmail.com>
2021-07-28 20:19:49 +0300

    repeated 2nd angular lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'atomangularfirstproject';
+  x = 10*100;
+
+  constructor() {
+    setTimeout(() => this.title = "ATOMSPACE", 2000)
+  }
+
+  sayHello(name: string) {
+    return `Hello, ${name}`;
+  }
 }
---
+++ src/app/components/a/a.component.ts
@@ -0,0 +1,16 @@
+import { ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.component.html',
+  styleUrls: ['./a.component.scss']
+})
+export class AComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
